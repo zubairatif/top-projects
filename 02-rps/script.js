16eae7b06739cb938c233efb096fc0fec8862a4c
Zubair Atif <zubair.atif@gmail.com>
2023-02-19 21:39:07 +0000

    completed logic of rps UI

--- 02-rps/script.js
+++ 02-rps/script.js
@@ -1,53 +1,71 @@
 const pScore = document.getElementById("pScore");
 const cScore = document.getElementById("cScore");
-const choices = ["rock", "paper", "scissors"];
+const roundResultDisplay = document.getElementById("roundResultDisplay");
+const totalGamesDisplay = document.getElementById("totalGames");
+const gamesWonDisplay = document.getElementById("gamesWon");
+const buttons = document.querySelectorAll(".options");
+const choices = ["rock", "paper", "scissor"];
 let computerSelection;
 let playerSelection;
 let computerScore = 0;
 let playerScore = 0;
-function getComputerChoice() {
-  return choices[Math.floor(Math.random() * choices.length)];
-}
-function getPlayerChoice() {
-  while (0 == 0) {
-    playerSelection = prompt("Choose: ").toLowerCase();
-    if (
-      playerSelection === "rock" ||
-      playerSelection === "paper" ||
-      playerSelection === "scissors"
-    ) {
-      break;
-    }
-  }
-  return playerSelection;
-}
-function playRound(playerSelection, computerSelection) {
+let totalGames = 0;
+let gamesWon = 0;
+let gameResult;
+function game(playerSelection) {
+  let computerSelection = choices[Math.floor(Math.random() * choices.length)];
   if (playerSelection.toLowerCase() === computerSelection) {
-    return "tie";
+    roundResultDisplay.innerText = "It's a Tie";
   } else if (
     (playerSelection.toLowerCase() === "rock" &&
-      computerSelection === "scissors") ||
+      computerSelection === "scissor") ||
     (playerSelection.toLowerCase() === "paper" &&
       computerSelection === "rock") ||
-    (playerSelection.toLowerCase() === "scissors" &&
+    (playerSelection.toLowerCase() === "scissor" &&
       computerSelection === "paper")
   ) {
-    return "win";
+    roundResultDisplay.innerText = "You Won!";
+    playerScore++;
+    pScore.innerText = String(playerScore);
   } else {
-    return "lose";
+    roundResultDisplay.innerText = "You Lost";
+    computerScore++;
+    cScore.innerText = String(computerScore);
+  }
+  if (computerScore >= 5 || playerScore >= 5) {
+    let displayResult = document.createElement("p");
+    if (computerScore > playerScore) {
+      displayResult.appendChild(document.createTextNode("You Lost the Game!"));
+    } else {
+      displayResult.appendChild(document.createTextNode("You Won the game!"));
+      gamesWon++;
+    }
+    roundResultDisplay.remove();
+    document.body.appendChild(displayResult);
+    totalGames++;
+    totalGamesDisplay.innerText = String(totalGames);
+    gamesWonDisplay.innerText = String(gamesWon);
+    computerScore = 0;
+    playerScore = 0;
+    return;
   }
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    result = playRound(getPlayerChoice(), getComputerChoice());
-    if (result === "win") {
-      playerScore++;
-      pScore.innerText = String(playerScore);
-    }
-    if (result === "lose") {
-      computerScore++;
-      cScore.innerText = String(computerScore);
-    }
-  }
-}
+// function game(playerChoice) {
+// while (computerScore < 5 && playerScore < 5) {
+//   result = playRound(playerChoice);
+//   if (result === "win") {
+//     playerScore++;
+//     pScore.innerText = String(playerScore);
+//   }
+//   if (result === "lose") {
+//     computerScore++;
+//     cScore.innerText = String(computerScore);
+//   }
+// }
+
+// }
+
+buttons.forEach((item) => {
+  item.addEventListener("click", (btn) => game(btn.target.dataset.weapon));
+});
