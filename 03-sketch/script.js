752659279b3592eea6c86c6fea327d4b74913a47
Zubair Atif <zubair.atif@gmail.com>
2023-02-26 11:45:22 +0000

    fixed bugs in 03-sketch

--- 03-sketch/script.js
+++ 03-sketch/script.js
@@ -1,21 +1,27 @@
 const container = document.getElementById("container");
 const colorPicker = document.getElementById("colorPicker");
+const rainbowBtn = document.getElementById("rainbowBtn");
+const eraserBtn = document.getElementById("eraserBtn");
+const singleBtn = document.getElementById("singleBtn");
+
 let rainbowMode = false;
 let eraserMode = false;
-letcolorMode = false;
+let colorMode = false;
+
 let gridNumber = 16;
 let dimensions = 960 / gridNumber;
-let color = colorPicker.value;
-colorPicker.addEventListener("change", (e) => (color = e.target.value));
 
-const squares = document.querySelectorAll(".gridSquare");
-function eraseAll() {
-  squares.forEach((element) => (element.style.backgroundColor = "#ffffff"));
-}
+let color = colorPicker.value;
+
+colorPicker.addEventListener("change", (e) => (color = e.target.value));
 function randomColor() {
   return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 function createGrid(squareCount) {
+  eraserMode = false;
+  rainbowMode = false;
+  rainbowBtn.classList.remove("active");
+  eraserBtn.classList.remove("active");
   container.innerHTML = "";
   for (let i = 1; i <= squareCount * squareCount; i++) {
     let gridSquare = document.createElement("div");
@@ -50,16 +56,44 @@ function askCreate() {
 function toggleRainbowMode() {
   if (!rainbowMode) {
     rainbowMode = true;
+    rainbowBtn.classList.add("active");
+  } else {
+    rainbowBtn.classList.remove("active");
+  }
+  if (eraserMode) {
+    eraserMode = false;
+    eraserBtn.classList.remove("active");
   }
 }
 
 function toggleEraser() {
   if (!eraserMode) {
     eraserMode = true;
+    eraserBtn.classList.add("active");
+  } else {
+    toggleColorMode();
+    eraserBtn.classList.remove("active");
+  }
+  if (rainbowMode) {
+    rainbowMode = false;
+    rainbowBtn.classList.remove("active");
   }
 }
 function toggleColorMode() {
-  eraserMode = false;
-  rainbowMode = false;
+  if (!colorMode) {
+    colorMode = true;
+    singleBtn.classList.add("active");
+    eraserMode = false;
+    rainbowMode = false;
+    rainbowBtn.classList.remove("active");
+    eraserBtn.classList.remove("active");
+  } else {
+    singleBtn.classList.remove("active");
+  }
+}
+function eraseAll() {
+  document.querySelectorAll(".gridSquare").forEach((element) => {
+    element.style.backgroundColor = "#ffffff";
+  });
 }
 createGrid(gridNumber);
