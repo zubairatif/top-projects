0081f0bbdbe9edd35b70287d625e601c44ac5400
Zubair Atifgit config --global user.email zubair.atif@@gmail.comgit config --global user.name Zubair <zubair.atif@gmail.com>
2023-03-25 22:53:07 +0000

    working Library without read btn

--- 07_library/script.js
+++ 07_library/script.js
@@ -1,20 +1,95 @@
-const newBookBtn = document.querySelector(".newBookBtn");
-newBookBtn.addEventListener("click", (e) => {});
-
 let myLibrary = [];
+const main = document.getElementById("main");
+const addNewBtn = document.getElementById("addNewBtn");
+const formContainer = document.getElementById("formContainer");
+const form = document.getElementById("form");
+const formTitle = document.getElementById("title");
+const formAuthor = document.getElementById("author");
+const formPages = document.getElementById("pages");
+const formCheckbox = document.getElementById("haveRead");
+const submitBookBtn = document.getElementById("submitBookbtn");
+const overlay = document.querySelector(".overlay");
+let title;
+let author;
+let pages;
+let haveRead;
+addNewBtn.addEventListener("click", function () {
+  formContainer.classList.toggle("show");
+  overlay.classList.toggle("show");
+  formTitle.value = "";
+  formAuthor.value = "";
+  formPages.value = "";
+});
+document.addEventListener("click", (event) => {
+  if (event.target === overlay) {
+    formContainer.classList.toggle("show");
+    overlay.classList.toggle("show");
+  }
+});
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  title = formTitle.value;
+  author = formAuthor.value;
+  pages = formPages.value;
+  if (formCheckbox.checked) {
+    haveRead = "yes";
+  } else {
+    haveRead = "no";
+  }
+  let book1 = new Book(title, author, pages, haveRead);
+  formContainer.classList.toggle("show");
+  overlay.classList.toggle("show");
+  if (bookExists(book1)) return;
+  myLibrary.push(book1);
+  main.innerHTML = " ";
+  addBookToLibrary(myLibrary);
+});
+function bookExists(book) {
+  return myLibrary.some(function (existing) {
+    return existing.title === book.title && existing.author === book.author;
+  });
+}
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.haveRead = read;
   this.info = function () {
     return `${title} by ${author}. ${pages} pages, ${read}`;
   };
 }
-const book1 = new Book("Science", "Messi", 800, "read");
-const book2 = new Book("Chemistry", "Cristiano", 532, "not read");
-const book3 = new Book("Physics", "Neymar", 200, "read");
-const book4 = new Book("Maths", "Haaland", 450, "not read");
-const book5 = new Book("Computer", "AI", 1500, "read");
-function addBookToLibrary() {}
+
+function addBookToLibrary(books) {
+  for (let i = 0; i < books.length; i++) {
+    let book = books[i];
+    createCard(book.title, book.author, book.pages, book.haveRead, i);
+  }
+}
+function createCard(title, author, pages, haveRead, bookNumber) {
+  let cardContainer = document.createElement("div");
+  cardContainer.classList.add("cardContainer");
+  cardContainer.dataset.index = `${bookNumber}`;
+  let cardTitle = document.createElement("h2");
+  cardTitle.innerText = `${title}`;
+  let cardAuthor = document.createElement("h3");
+  cardAuthor.innerText = `${author}`;
+  let cardPages = document.createElement("p");
+  cardPages.innerText = `${pages}`;
+  let cardRemove = document.createElement("button");
+  cardRemove.innerText = "Remove";
+  cardRemove.addEventListener("click", (e) => {
+    e.target.parentElement.style.display = "none";
+    myLibrary.splice(bookNumber, 1);
+  });
+  cardContainer.appendChild(cardTitle);
+  cardContainer.appendChild(cardAuthor);
+  cardContainer.appendChild(cardPages);
+  if (haveRead === "yes") {
+    let cardRead = document.createElement("h4");
+    cardRead.innerText = `Read`;
+    cardContainer.appendChild(cardRead);
+  }
+  cardContainer.appendChild(cardRemove);
+  main.appendChild(cardContainer);
+}
