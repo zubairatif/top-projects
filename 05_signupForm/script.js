431da7bb5888a8d83fb823ef104e99409147b490
Zubair Atifgit config --global user.email zubair.atif@@gmail.comgit config --global user.name Zubair <zubair.atif@gmail.com>
2023-04-23 20:25:17 +0100

    Fixed tic tac toe bug

--- 05_signupForm/script.js
+++ 05_signupForm/script.js
@@ -1,5 +1,10 @@
+const errorDisplay = document.querySelector(".error");
+const email = document.querySelector("#email");
+const zipCode = document.querySelector("#zipCode");
+const phone = document.querySelector("#phone");
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirm");
+let message;
 // ^ - Start of the string
 // (?=.*\d) at least 1 digit (0-9)
 // (?=.*[a-z]) at least 1 lowercase letter (a-z)
@@ -8,11 +13,11 @@ const confirmPassword = document.querySelector("#confirm");
 // (?!.*\s) no space characters
 // .{8,} - at least 8 characters
 // $ - End of the string
-const pattern =
+const passwordPattern =
   /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$/;
 
 password.addEventListener("input", function () {
-  if (password.value.match(pattern)) {
+  if (password.value.match(passwordPattern)) {
     password.setCustomValidity("");
   } else {
     password.setCustomValidity("Invalid");
@@ -26,3 +31,27 @@ confirmPassword.addEventListener("input", function () {
     confirmPassword.setCustomValidity("Invalid");
   }
 });
+
+email.addEventListener("input", () => {
+  if (email.validity.typeMismatch) {
+    message = "Please use the format user@domain.xyz";
+    email.setCustomValidity(message);
+    updateErrorDisplay("email", message);
+  } else {
+    email.setCustomValidity("");
+  }
+});
+
+zipCode.addEventListener("input", () => {
+  if (email.validity.typeMismatch) {
+    message = "Enter 6 or 7 digit zip code";
+    email.setCustomValidity(message);
+    updateErrorDisplay("email", message);
+  } else {
+    email.setCustomValidity("");
+  }
+});
+
+function updateErrorDisplay(element, message) {
+  errorDisplay.textContent = `Error in ${element}: ${message}`;
+}
--- 08_ticTacToe/script.js
+++ 08_ticTacToe/script.js
@@ -161,7 +161,6 @@ const Game = (() => {
     return emptyBoxes[boxChosen];
   };
   function minimax(board, depth, isMaxPlayer, alpha, beta) {
-    const boardCacheKey = getBoardCacheKey(board);
     if (checkForWin(board, players[0].mark, true)) {
       return -10 - depth;
     } else if (checkForWin(board, players[1].mark, true)) {
